f8cf847ba464a20ad911a40c9fc67749fd650188
rcyerg <rcyerger02@gmail.com>
2024-03-12 10:22:03 -0400

    Initialized Repo

---
+++ src/main/java/Demo/PhraseOMatic.java
@@ -0,0 +1,20 @@
+package Demo;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+
+        String [] wordListOne = {"24/7", "multi-Tier", "30_000 foot"};
+        String [] wordListTwo = {"empowered", "sticky", "value-added"};
+        String [] wordListThree = {"process", "tipping-point", "solution"};
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        System.out.println("What we need is a " + phrase);
+    }
+}
